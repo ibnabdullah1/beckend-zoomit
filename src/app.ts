bfe517e898f774067acd981bb7921d0012553483
ibnabdullah1 <arfathossen541@gmail.com>
2025-05-25 12:10:21 +0600

    fixed cor error

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,12 @@ import { UploadRoutes } from "./app/routes/upload.route";
 const app: Application = express();
 
 // Middleware setup
-app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://zoom-it.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
