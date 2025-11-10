3d84e603a6eeac988b5ac6817efdefaea9ec0c2f
ibnabdullah1 <arfathossen541@gmail.com>
2025-11-10 08:32:52 +0600

    blog slug generateslug update

--- src/app/utils/slug.ts
+++ src/app/utils/slug.ts
@@ -1,5 +1,10 @@
 import slugify from "slugify";
 
 export const generateSlug = (text: string): string => {
-  return slugify(text, { lower: true, strict: true });
+  return slugify(text, {
+    lower: true,
+    strict: true,
+    trim: true,
+    remove: /[*+~.()'"!:@]/g,
+  });
 };
