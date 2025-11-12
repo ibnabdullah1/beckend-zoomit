e074bacddce91c8c65723282dceedb46467f67ff
ibnabdullah1 <arfathossen541@gmail.com>
2025-11-12 06:38:55 +0600

    slug ganerated func updated

--- src/app/utils/slug.ts
+++ src/app/utils/slug.ts
@@ -1,10 +1,11 @@
 import slugify from "slugify";
 
-export const generateSlug = (text: string): string => {
-  return slugify(text, {
+export const generateSlug = (text: string) => {
+ const rawSlug =  slugify(text, {
     lower: true,
     strict: false,
     trim: true,
     remove: /[*+~.()'"!:@]/g,
   });
+   return encodeURIComponent(rawSlug);
 };
